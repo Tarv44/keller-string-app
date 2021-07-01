11c1f50033d87e62f35bcd8fd8db9a248b6aa358
Jonah Tarver <jonahdevine@gmail.com>
2021-07-01 15:39:39 -0700

    Fix Stripe() error

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -6,7 +6,7 @@
  let stripePromise
  const getStripe = () => {
    if (!stripePromise) {
-     stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+     stripePromise = loadStripe('pk_test_51J7TS2Iw7CyPxjdbX5Y1fECXOwYNIM55CMeyGp5gA6e2tjwhLnYrLtROwoUgS44ZeSkFt3y7TrAOhceMSXdjgJ9300k7etZzyy')
    }
    return stripePromise
  }
