11394f63a01f64f73521fbd0d10f663ad61601be
Jonah Tarver <jonahdevine@gmail.com>
2021-08-11 18:19:25 -0700

    Use loadable with youtube

--- src/components/LandingPitch.js
+++ src/components/LandingPitch.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import styled from 'styled-components';
 import {Link as GLink} from 'gatsby';
 import loadable from '@loadable/component';
-const LandingVideo = loadable(() => import('./LandingVideo'));
+const LandingVideo = lazy(() => import('./LandingVideo'));
 const Carousel = loadable(() => import('./Carousel'));
 const Button = loadable(() => import('./styled/Button'));
 const Hr = loadable(() => import('./styled/Hr'));
